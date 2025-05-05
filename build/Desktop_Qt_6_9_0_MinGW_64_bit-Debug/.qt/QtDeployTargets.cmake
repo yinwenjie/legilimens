6d7ae1af853fa05d37bf8cf6eca710a85de2b441
yinwenjie <eternal.ywj@gmail.com>
2025-05-05 20:30:02 +0800

    Initial commit

---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_legilimens_FILE E:/Code/QT/legilimens/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/legilimens.exe)
+set(__QT_DEPLOY_TARGET_legilimens_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_legilimens_RUNTIME_DLLS D:/Developer/QT/6.9.0/mingw_64/bin/Qt6Widgets.dll;D:/Developer/QT/6.9.0/mingw_64/bin/Qt6Gui.dll;D:/Developer/QT/6.9.0/mingw_64/bin/Qt6Core.dll)
